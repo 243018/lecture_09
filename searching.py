46e267705a94a1dfc8aedfb275bf11b1884b7c02
Dominik Bachraty <dominik.ba123@gmail.com>
2025-04-10 08:56:16 +0200

    added linear_search

--- searching.py
+++ searching.py
@@ -24,9 +24,34 @@ def read_data(file_name, field):
     return dictionary[field]
 
 
+def linear_search(sequence, number):
+    """
+    linear search algorithm implementation
+    :param sequence: (list), searched sequence
+    :param number: (int), number to be found
+    :return: (dict)
+    """
+
+    count = 0
+    positions = []
+
+    for i in range(len(sequence)):
+        if sequence[i] == number:
+            positions.append(i)
+            count += 1
+
+    return {
+        "positions": positions,
+        "count": count
+    }
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
+    number = 5
+    linear_result = linear_search(sequential_data, number)
+    print(linear_result)
 
 
 if __name__ == '__main__':
