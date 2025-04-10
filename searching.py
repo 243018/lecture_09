65fa2db0861e64e4b8082d028be8715119135805
Dominik Bachraty <dominik.ba123@gmail.com>
2025-04-10 09:27:47 +0200

    added pattern_search

--- searching.py
+++ searching.py
@@ -27,7 +27,7 @@ def read_data(file_name, field):
 def linear_search(sequence, number):
     """
     linear search algorithm implementation
-    :param sequence: (list), searched sequence
+    :param sequence: (list or str), searched sequence
     :param number: (int), number to be found
     :return: (dict)
     """
@@ -46,12 +46,43 @@ def linear_search(sequence, number):
     }
 
 
+def pattern_search(sequence, pattern):
+    """
+    sequential searching algorithm implementation
+    :param sequence: (list or str), searched sequence
+    :param pattern: (str), pattern to be found
+    :return: (set), positions, where pattern was found within sequence
+    """
+
+    pattern_length = len(pattern)
+    appearance = set()
+    pattern_idx_right = pattern_length
+    pattern_idx_left = 0
+
+    while pattern_idx_right < len(sequence):
+        for idx_help in range(pattern_length):
+            if pattern[idx_help] != sequence[pattern_idx_left + idx_help]:
+                break
+            else:
+                appearance.add(pattern_idx_left + pattern_length // 2)
+            pattern_idx_left += 1
+            pattern_idx_right += 1
+    print(appearance)
+    return appearance
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
+
     number = 5
     linear_result = linear_search(sequential_data, number)
     print(linear_result)
+
+    pattern = "ATA"
+    sequential_data2 = read_data("sequential.json", "dna_sequence")
+    pattern_result = pattern_search(sequential_data2, pattern)
+    print(pattern_result)
 
 
 if __name__ == '__main__':
